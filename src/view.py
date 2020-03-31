4cf7af14b0378f0c5bea05bbd8d73d33c2c8b9fb
cemondr <cemconder@gmail.com>
2020-03-31 04:25:04 -0700

    rendering edit

--- src/view.py
+++ src/view.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('main.html')
+    return render_template('main.html', status=None)
 
 @app.route('/uploaded')
 def  allow_click():
-    return "page with uploaded photo"
+    return render_template('main.html',status="uploaded")
 
 @app.route('/result')
 def get_your_result():
-    return "page that returns your RGB value"
+    return render_template('main.html', status="resulted")
 
 
 
