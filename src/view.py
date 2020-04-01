a6eda2fb3dbef493b4db99f90f9de758cd4d4252
cemondr <cemconder@gmail.com>
2020-04-01 10:59:01 -0700

    upload works

--- src/view.py
+++ src/view.py
@@ -1,15 +1,40 @@
-from flask import Flask
+from flask import Flask,flash,request,redirect,url_for,send_from_directory
 from flask import render_template
+from werkzeug.utils import secure_filename
+import os
+import sys
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
+app.secret_key = os.urandom(24)
 
-@app.route('/')
+uploads_dir = os.path.join(app.root_path,'static')
+os.makedirs(uploads_dir,exist_ok=True)
+app.config['upload_dir'] = uploads_dir
+
+@app.route('/', methods=['GET','POST'])
 def main_page():
-    return render_template('main.html', status=None)
+    if request.method == 'GET':
+        return render_template('main.html', status=None)
+    if request.method == 'POST':
+        # Check if file is in the request. If not flash a message
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url) #Redirect to the same url
 
-@app.route('/uploaded')
-def  allow_click():
-    return render_template('main.html',status="uploaded")
+        file = request.files['file']
+        #Browswer might submit an empty part without filename
+        if file.filename == '':
+            flash('Empty file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(uploads_dir, filename))
+            return redirect(url_for('allow_click', filename=filename))
+
+@app.route('/uploaded/<filename>')
+def  allow_click(filename):
+    img_url= url_for('static',filename=filename)
+    return render_template('main.html',status="uploaded",img_url=img_url)
 
 @app.route('/resulted')
 def get_your_result():
